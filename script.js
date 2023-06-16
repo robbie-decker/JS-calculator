0d69008ad8a5dcf399cd63706c2f146ebc4b7d6b
Robbie Decker <robbd56@gmail.com>
2023-06-15 20:49:11 -0400

    Initial improvements

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
+const numbers = document.querySelectorAll('.numbers');
+const clearButton = document.getElementById("clear");
+const display = document.getElementById('display');
+
+let displayNumber;
+
+// we use the .forEach method to iterate through each number
+numbers.forEach((number) => {
+  // and for each one we add a 'click' listener
+  number.addEventListener('click', () => {
+  display.textContent = display.textContent + number.textContent;
+  displayNumber = +display.textContent
+  console.log(displayNumber);
+  });
+});
+
+clearButton.addEventListener('click', () => {
+  display.textContent = "";
+  displayNumber = 0;
+});
+
+
 function add(a, b) {
     return a + b;
-  };
+}
   
 function subtract(a, b) {
     return a - b;
-  };
+}
   
 function sum(array) {
     return array.reduce((total, current) => total + current, 0);
-  };
+}
   
 function multiply(array) {
-    return array.length
-      ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
-      : 0;
-  };
-  
-function power(a, b) {
-    return Math.pow(a, b);
-  };
+   return a * b;
+}
 
-  // This is another implementation of Factorial that uses recursion
-  // THANKS to @ThirtyThreeB!
-function factorial(n) {
-    if (n === 0) {
-      return 1;
-    }
-    return n * recursiveFactorial(n - 1);
-  };
+function divide(a, b){
+    return a / b;
+}
 
-  
+function operate(operator, a, b){
+  switch(operator){
+    case "+":
+      add(a,b)
+      break;
+    case "-":
+      subtract(a,b);
+      break;
+    case "*":
+      multiply(a,b)
+      break;
+    case "/":
+      divide(a,b)
+      break;
+  }
+}
+
+// 
